ad8ab79ee3fcb073ab159cc827e5a00102746b97
Manasa Yadavalli <ymanasa@umich.edu>
2024-05-15 10:22:35 -0400

    processed structures for docking

---
+++ structures/hscpl_5MAJ_20231023/1_make_structure.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# This template downloads a structure file from the protein databank
+# and splits it into the receptor (rec.pdb) and the reference ligand (xtal-lig.pdb)
+#
+# The reference ligand is used for sampling. The atoms of a candidate ligand
+# are matched to the atoms of the reference ligand. Alternatively, the binding-site
+# can be flooded with generated spehres e.g. "sph-gen".
+
+# fill in 
+PDB_CODE=5MAJ
+RECEPTOR_CHAIN=A
+LIGAND_CHAIN=A
+LIGAND_RESID=7KH
+
+wget https://files.rcsb.org/download/${PDB_CODE}.pdb
+mv ${PDB_CODE}.pdb raw.pdb
+
+grep "^ATOM.................${RECEPTOR_CHAIN}" raw.pdb > rec.pdb
+grep "^HETATM...........${LIGAND_RESID} ${LIGAND_CHAIN}" raw.pdb > xtal-lig.pdb
+
+
+echo "N atoms in rec.pdb: $(wc -l < rec.pdb)"
+echo "N atoms in xtal-lig.pdb: $(wc -l < xtal-lig.pdb)"
