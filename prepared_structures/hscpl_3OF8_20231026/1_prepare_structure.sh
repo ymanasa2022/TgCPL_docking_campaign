7aec44c2a11701f9a91c07b74e9a7e9bbf780f45
Manasa Yadavalli <ymanasa@umich.edu>
2024-05-15 10:44:45 -0400

    hscpl_3OF8

---
+++ prepared_structures/hscpl_3OF8_20231026/1_prepare_structure.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -z ${DOCK_TEMPLATE+x} ]; then
+    echo "ERROR: The \${DOCK_TEMPLATE} variable is not set"
+    echo "ERROR: Please run 'source setup_dock_environment.sh' in the project root directory"
+fi
+
+#structure folder:
+STRUCTURE=$(readlink -f ../../structures/hscpl_3OF8_20231026)
+
+# enter residue number for covalent docking
+COVALENT_RESIDUE_NUMBER=26
+COVALENT_RESIDUE_NAME=CYS
+COVALENT_RESIDUE_ATOMS=HG
+
+echo "Selected residue for covalent docking:"
+echo "   residue nubmer: ${COVALENT_RESIDUE_NUMBER}"
+echo "   residue name:   ${COVALENT_RESIDUE_NAME}"
+echo "   covalent atoms:  ${COVALENT_RESIDUE_ATOMS}"
+
+source ${DOCK_TEMPLATE}/scripts/dock_clean.sh
+
+echo 'Preparing receptor and xtal-lig ...'
+cp ${STRUCTURE}/rec.pdb rec.pdb
+cp ${STRUCTURE}/xtal-lig.pdb xtal-lig.pdb
+
+# run blastermaster standard to protonate the receptor
+# then use the protonated receptor as the input to blastermaster covalent
+
